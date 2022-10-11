07e7be7a8f52b33f10d54eacc42d176f2435d3f9
patrickmao <patrickmao93@gmail.com>
2022-10-11 17:43:20 +0800

    use custom json rpc provider

--- packages/sdk-api-example/src/server.js
+++ packages/sdk-api-example/src/server.js
@@ -1,90 +1,110 @@
-const express = require('express')
-const { Hop } = require('@hop-protocol/sdk')
+const express = require("express");
+const { Hop } = require("@hop-protocol/sdk");
 
-const port = process.env.PORT || 8000
-const app = express()
-const hop = new Hop('mainnet')
+const port = process.env.PORT || 8000;
+const app = express();
+const hop = new Hop("mainnet");
 
-app.get('/quote', async (req, res) => {
-  const { amount, token, fromChain, toChain, slippage } = req.query
+hop.setChainProviders({
+  ethereum: new providers.StaticJsonRpcProvider(
+    "https://eth-mainnet.alchemyapi.io/v2/imF7XWfmReAqqvyVafPOSJxDtY8U8_ZT"
+  ),
+  polygon: new providers.StaticJsonRpcProvider(
+    "https://bold-winter-surf.matic.quiknode.pro/9c226c540f9c66f92f4ef21af6e3dfa257c91d68/"
+  ),
+  optimism: new providers.StaticJsonRpcProvider(
+    "https://optimism-mainnet.infura.io/v3/029bfb981dc748b1affa9e77eb0e5477"
+  ),
+  arbitrum: new providers.StaticJsonRpcProvider(
+    "https://arb-mainnet.g.alchemy.com/v2/imF7XWfmReAqqvyVafPOSJxDtY8U8_ZT"
+  ),
+});
+
+app.get("/quote", async (req, res) => {
+  const { amount, token, fromChain, toChain, slippage } = req.query;
 
   try {
-    const bridge = hop.bridge(token)
-    const amountBn = bridge.parseUnits(amount)
-    const data = await bridge.getSendData(amountBn, fromChain, toChain)
-    const { totalFee, amountOut, estimatedReceived } = data
-    const amountOutMin = bridge.calcAmountOutMin(amountOut, slippage)
+    const bridge = hop.bridge(token);
+    const amountBn = bridge.parseUnits(amount);
+    const data = await bridge.getSendData(amountBn, fromChain, toChain);
+    const { totalFee, amountOut, estimatedReceived } = data;
+    const amountOutMin = bridge.calcAmountOutMin(amountOut, slippage);
 
     res.json({
       bonderFee: totalFee.toString(),
       amountOutMin: amountOutMin.toString(),
-      estimatedRecieved: estimatedReceived.toString()
-    })
+      estimatedRecieved: estimatedReceived.toString(),
+    });
   } catch (err) {
-    res.json({ error: err.message })
+    res.json({ error: err.message });
   }
-})
+});
 
-app.get('/status', async (req, res) => {
-  const { token, transferId, transactionHash } = req.query
+app.get("/status", async (req, res) => {
+  const { token, transferId, transactionHash } = req.query;
 
   try {
-    const bridge = hop.bridge(token)
-    const status = await bridge.getTransferStatus(transferId || transactionHash)
-    res.json(status)
+    const bridge = hop.bridge(token);
+    const status = await bridge.getTransferStatus(
+      transferId || transactionHash
+    );
+    res.json(status);
   } catch (err) {
-    res.json({ error: err.message })
+    res.json({ error: err.message });
   }
-})
+});
 
-app.get('/build-tx', async (req, res) => {
-  const { token, amount, fromChain, toChain, recipient } = req.query
+app.get("/build-tx", async (req, res) => {
+  const { token, amount, fromChain, toChain, recipient } = req.query;
 
   try {
-    const bridge = hop.bridge(token)
-    const amountBn = bridge.parseUnits(amount)
+    const bridge = hop.bridge(token);
+    const amountBn = bridge.parseUnits(amount);
     const tx = await bridge.populateSendTx(amountBn, fromChain, toChain, {
-      recipient
-    })
+      recipient,
+    });
     res.json({
-      tx
-    })
+      tx,
+    });
   } catch (err) {
-    res.json({ error: err.message })
+    res.json({ error: err.message });
   }
-})
+});
 
-app.get('/approval/check-allowance', async (req, res) => {
-  const { token, amount, fromChain, account } = req.query
+app.get("/approval/check-allowance", async (req, res) => {
+  const { token, amount, fromChain, account } = req.query;
 
   try {
-    const bridge = hop.bridge(token)
-    const amountBn = bridge.parseUnits(amount)
-    const needsApproval = await bridge.needsApproval(amountBn, fromChain, account)
+    const bridge = hop.bridge(token);
+    const amountBn = bridge.parseUnits(amount);
+    const needsApproval = await bridge.needsApproval(
+      amountBn,
+      fromChain,
+      account
+    );
     res.json({
-      needsApproval
-    })
+      needsApproval,
+    });
   } catch (err) {
-    res.json({ error: err.message })
+    res.json({ error: err.message });
   }
-})
+});
 
-app.get('/approval/build-tx', async (req, res) => {
-  const { token, amount, fromChain } = req.query
+app.get("/approval/build-tx", async (req, res) => {
+  const { token, amount, fromChain } = req.query;
 
   try {
-    const bridge = hop.bridge(token)
-    const amountBn = bridge.parseUnits(amount)
-    const tx = await bridge.populateSendApprovalTx(amountBn, fromChain)
+    const bridge = hop.bridge(token);
+    const amountBn = bridge.parseUnits(amount);
+    const tx = await bridge.populateSendApprovalTx(amountBn, fromChain);
     res.json({
-      tx
-    })
+      tx,
+    });
   } catch (err) {
-    res.json({ error: err.message })
+    res.json({ error: err.message });
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
-
+  console.log(`listening on port ${port}`);
+});
